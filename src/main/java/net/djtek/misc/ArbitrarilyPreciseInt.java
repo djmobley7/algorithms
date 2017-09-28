c7d58a74ae8463c9f46362e485e3bc3f7a312b85
Daniel Mobley <djmobley7@gmail.com>
2017-09-27 20:43:13 -0400

    add more methods and junit to ArbitrarilyPreciseInt class

--- src/main/java/net/djtek/misc/ArbitrarilyPreciseInt.java
+++ src/main/java/net/djtek/misc/ArbitrarilyPreciseInt.java
@@ -4,10 +4,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ArbitrarilyPreciseInt {
+    // values are stored in reverse order e.g. 123456 is stored as 654321
     private List<Integer> values;
 
     /**
-     * Create an arbitrarily precise, non-negative integer from a string
+     * Create an arbitrarily precise, non-negative integer from a string.  The string
+     * may contain underscores to improve readability for large values e.g. 1_000_000
      * @param str The string input
      */
     public ArbitrarilyPreciseInt(String str){
@@ -17,9 +19,17 @@ public class ArbitrarilyPreciseInt {
 
         values = new ArrayList<>();
         for (int i = 0; i < str.length(); ++i){
-            int value = Character.getNumericValue(str.charAt(i));
+            char ch = str.charAt(i);
+
+            // skip underscores
+            if (ch == '_') {
+                continue;
+            }
+
+            // parse numeric value
+            int value = Character.getNumericValue(ch);
             if (value >= 0){
-                values.add(value);
+                values.add(0, value);
             } else {
                 throw new IllegalArgumentException(str + " is not a number");
             }
@@ -28,17 +38,59 @@ public class ArbitrarilyPreciseInt {
 
     /**
      * Increment by 1
+     * @return The object
      */
-    public void increment(){
-        // TODO
+    public ArbitrarilyPreciseInt increment(){
+        return increment(1);
+    }
+
+    /**
+     * Return primitive int value
+     * @return int value
+     * @throws NumberFormatException if too large
+     */
+    public int toInt(){
+        return Integer.parseInt(toString());
     }
 
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
         for (Integer value : values){
-            sb.append(value);
+            sb.insert(0, value);
         }
         return sb.toString();
+    }
+
+    private ArbitrarilyPreciseInt increment(int amount){
+        assert amount >= 0 && amount < 9 : "increment amount should be between 0 and 9";
+        assert values.size() > 0 : "values size should be > 0";
+
+        int index = 0;
+
+        int digit = values.get(index) + amount;
+        int carry = 0;
+        if (digit >= 10){
+            carry = digit / 10;
+            digit %= 10;
+        }
+        values.set(index, digit);
+
+        while (carry > 0){
+            ++index;
+            if (index > values.size() - 1){
+                values.add(0);
+            }
+
+            digit = values.get(index) + carry;
+            carry = 0;
+            if (digit >= 10){
+                carry = digit / 10;
+                digit %= 10;
+            }
+            values.set(index, digit);
+        }
+
+        return this;
     }
 }
--- src/test/java/net/djtek/misc/ArbitrarilyPreciseIntTests.java
+++ src/test/java/net/djtek/misc/ArbitrarilyPreciseIntTests.java
@@ -3,9 +3,35 @@ package net.djtek.misc;
 import org.junit.*;
 import org.junit.rules.ExpectedException;
 
+import static org.junit.Assert.*;
+
 public class ArbitrarilyPreciseIntTests {
     @Rule
     public ExpectedException thrown = ExpectedException.none();
 
-    // TODO
+    @Test
+    public void incrementTests(){
+        assertEquals(2, new ArbitrarilyPreciseInt("1").increment().toInt());
+
+        // test carry over
+        assertEquals(10, new ArbitrarilyPreciseInt("9").increment().toInt());
+        assertEquals(1_000, new ArbitrarilyPreciseInt("999").increment().toInt());
+        assertEquals(1_000_000_000, new ArbitrarilyPreciseInt("999_999_999").increment().toInt());
+
+        // test carry over on really big integers
+        assertEquals("1000000000000", new ArbitrarilyPreciseInt("999_999_999_999").increment().toString());
+        assertEquals("1000000000000000000000", new ArbitrarilyPreciseInt("999_999_999_999_999_999_999").increment().toString());
+    }
+
+    @Test
+    public void toIntTests(){
+        thrown.expect(NumberFormatException.class);
+        new ArbitrarilyPreciseInt("1_000_000_000_000_000_000").toInt();
+    }
+
+    @Test
+    public void toStringTests(){
+        ArbitrarilyPreciseInt i = new ArbitrarilyPreciseInt("123456");
+        assertEquals("123456", i.toString());
+    }
 }
